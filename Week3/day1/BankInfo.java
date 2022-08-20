a744d364ca26c12ce4d1a89463b51f6011605b40
MahaGuruBackiyam <110719016+MahaGuruBackiyam@users.noreply.github.com>
2022-08-20 19:25:34 +0530

    Add files via upload
    Week 3 day1

---
+++ Week3/day1/BankInfo.java
@@ -0,0 +1,18 @@
+package Week3.day1;
+
+public class BankInfo {
+public void saving() {
+	System.out.println("Savings: 1000rs");
+}
+public void fixed() {
+	System.out.println("Fixed: 0rs");
+}
+public void deposit() {
+	System.out.println("Deposit in Bank: 0rs");
+}
+
+
+
+
+
+}
