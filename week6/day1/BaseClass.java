3b289c53de8c70baf1d8b3b71a01d8a4d6091e12
MahaGuruBackiyam <110719016+MahaGuruBackiyam@users.noreply.github.com>
2022-09-12 14:49:17 +0530

    Add files via upload

--- week6/day1/BaseClass.java
+++ week6/day1/BaseClass.java
@@ -1,34 +1,60 @@
 package com.leaftaps.project.base;
 
 import java.time.Duration;
-
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-
+import org.testng.annotations.*;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseClass {
 	public ChromeDriver driver;
-	
-@BeforeMethod
-public void beforeMethod() {
-	WebDriverManager.chromedriver().setup();
-	driver = new ChromeDriver();
-	driver.manage().window().maximize();
-	driver.get("http://leaftaps.com/opentaps/");
-	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-	
-	
+
+	@BeforeMethod
+	public void beforeMethod() {
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://leaftaps.com/opentaps/");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		System.out.println("BeforeMethod");
+
 	}
-@AfterMethod
-public void afterMethod() {
-	
-	driver.close();
-	
-	
-}
 
+	@AfterMethod
+	public void afterMethod() {
 
+		driver.close();
+		System.out.println("AfterMethod");
+
+	}
+
+	@AfterSuite
+	public void afterSuite() {
+		System.out.println("AfterSuite");
+	}
+
+	@AfterTest
+	public void afterTest() {
+		System.out.println("AfterTest");
+	}
+
+	@AfterClass
+	public void afterClass() {
+		System.out.println("AfterClass");
+	}
+
+	@BeforeClass
+	public void beforeClass() {
+		System.out.println("BeforeClass");
+	}
+
+	@BeforeSuite
+	public void beforeSuite() {
+		System.out.println("BeforeSuite");
+	}
+
+	@BeforeTest
+	public void beforeTest() {
+		System.out.println("BeforeTest");
+	}
 
 }
